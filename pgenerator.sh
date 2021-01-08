7948f05032c150fafee0b65e7dab4d886842071f
Alvatador <69437783+Alvatador@users.noreply.github.com>
2021-01-08 20:52:01 +0300

    Update pgenerator.sh

--- pgenerator.sh
+++ pgenerator.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 # Colors
-white="\033[1;37m"
-grey="\033[0;37m"
-purple="\033[0;35m"
 red="\033[1;31m"
 green="\033[1;32m"
-yellow="\033[1;33m"
 purple="\033[0;35m"
-cyan="\033[0;36m"
-cafe="\033[0;33m"
-fiuscha="\033[0;35m"
 blue="\033[1;34m"
-tp="\e[0m"
 
 echo -e "$red"
 echo -n "Parola Üreticisine Hoşgeldiniz! Parolanızın Kaç Basamaklı Olmasını İstersiniz: "
